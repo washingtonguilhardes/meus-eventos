8d7de82f84ec8848456983d2bac541ff7884b529
Washington Freitas <junior201110@gmail.com>
2017-10-10 04:26:19 -0300

    add push notifications for feedback ok and not ok

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Progresive Web App, o que é.
 - Os Progressive Web Apps são um conjunto de técnicas para desenvolver aplicações web, adicionando progressivamente funcionalidades que antes só eram possíveis em apps nativos.
 
 - Recursos nativos aqui usado:
-    -Geolocalização, Camera, Notificação do tipo Push.
+    - Geolocalização, Notificação do tipo Push.
+
 - Recursos de PWA:
      - App manifest. (manifest.json)
      - Offline Data.
--- js/pushNotification.js
+++ js/pushNotification.js
@@ -1,13 +1,32 @@
-const getPermition = ()=>{
+const getPermition = () => {
     return Notification.requestPermission()
 };
 
-const push = (params) =>{
+const push = (params) => {
     getPermition()
-        .then(status =>{
-
+        .then(status => {
+            const notfication = new Notification(params.title, params.options || {})
         })
-        .catch(e => console.log('ERROR TO GET NOTIFICATION',e))
+        .catch(e => console.log('ERROR TO GET NOTIFICATION', e))
 };
+$('button.ok').on('click', () => {
+    const params = {
+        title: "Confirmado",
+        options: {
+            body: "Obrigado por ir ao evento !",
+            icon:'images/icon.png'
+        }
+    };
+    push(params);
+});
 
-
+$('button.not-ok').on('click', () => {
+    const params = {
+        title: "Você não vai ?",
+        options: {
+            body: "Poxa... uma pena. Estavamos te esperando. :(\nFica para próxima :)",
+            icon:'images/close_red.png'
+        }
+    };
+    push(params);
+});
